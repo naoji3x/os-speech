92a74a2c4aa6045416a5594920de8464af33fedd
naoji3x <naoji.3x@gmail.com>
2025-09-30 22:26:45 +0900

    build: adjust print width and end of line

--- plugins/mac/osspeech/Package.swift
+++ plugins/mac/osspeech/Package.swift
@@ -2,24 +2,24 @@
 import PackageDescription
 
 let package = Package(
-    name: "OSSpeechMacOnly",
-    platforms: [
-        .macOS(.v11)
-    ],
-    products: [
-        // 出力ファイル名: libOSSpeech.dylib
-        .library(name: "OSSpeech", type: .dynamic, targets: ["OSSpeech"])
-    ],
-    targets: [
-        .target(
-            name: "OSSpeech",
-            // Sources/OSSpeech は iOS 側フォルダへの symlink にします
-            path: "Sources/OSSpeech",
-            // mac で必要なフレームワークをリンク
-            linkerSettings: [
-                .linkedFramework("Speech"),
-                .linkedFramework("AVFoundation")
-            ],
-        )
-    ]
+  name: "OSSpeechMacOnly",
+  platforms: [
+    .macOS(.v11)
+  ],
+  products: [
+    // 出力ファイル名: libOSSpeech.dylib
+    .library(name: "OSSpeech", type: .dynamic, targets: ["OSSpeech"])
+  ],
+  targets: [
+    .target(
+      name: "OSSpeech",
+      // Sources/OSSpeech は iOS 側フォルダへの symlink にします
+      path: "Sources/OSSpeech",
+      // mac で必要なフレームワークをリンク
+      linkerSettings: [
+        .linkedFramework("Speech"),
+        .linkedFramework("AVFoundation"),
+      ],
+    )
+  ]
 )
